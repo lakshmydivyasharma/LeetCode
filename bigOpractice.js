097d2be96768f29c75e0e1212679d005c8979184
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-06-22 22:25:20 -0400

    updated v

---
+++ bigOpractice.js
@@ -0,0 +1,57 @@
+function funChallenge(input){
+let a = 10; // O(1)
+a = 50 + 3; // O(1)
+
+for (let i = 0; i < input.length; i++){ // O(n)
+  anotherFunction(); // O(n)
+  let stranger = true; // O(n)
+  a++; // O(n)
+}
+return a; // O(n)
+}
+
+BIG O(3+4n
+  KEY TAKEAWAY: reduces down to O(N)
+
+
+
+
+
+
+function anotherChallenge(imput){
+  let a = 5;//O(1)
+  let b = 10; //O(1)
+  let c = 50;//O(1)
+}
+for(let i = 0; i < input; i++){
+  let x = i + 1; //O(n)
+  let y = i +2; //O(n)
+  let z = i + 3;//O(n)
+}
+
+for (let j = 0 ; j < imput; j++){
+  let p = j * 2; // O(n)
+  let p = j *2; // O(n)
+}
+let whoAmI = "i dont know"; //O(1)
+
+
+BIG O(4+5n)
+
+
+
+
+// Log all pairs of array
+const boxes = [1,2 3,4,5];
+
+// [1, 2] [1, 3] [1, 4] [1, 5] [2, 3]
+function getPairs(array){
+  let pairs = [];
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; J < array.length; j++){
+      console.log(array[i], array[j])
+    }
+  }
+}
+
+getPairs(boxes)
