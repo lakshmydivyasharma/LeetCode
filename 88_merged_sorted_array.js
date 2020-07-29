ddfb7af8971395b3ef2496a9e430bb003034056c
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-07-28 23:31:59 -0400

    updated code

---
+++ 88_merged_sorted_array.js
@@ -0,0 +1,63 @@
+https://leetcode.com/problems/merge-sorted-array/submissions/
+
+88. Merge Sorted Array
+Easy
+
+2315
+
+4231
+
+Add to List
+
+Share
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+
+
+Constraints:
+
+-10^9 <= nums1[i], nums2[i] <= 10^9
+nums1.length == m + n
+nums2.length == n
+Accepted
+612.2K
+Submissions
+1.6M
+Seen this question in a real interview before?
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    var first = m - 1;
+    var second = n - 1;
+    for (var i = m + n - 1; i >= 0; i--) {
+        if (second < 0) {
+            break;
+        }
+        if (nums1[first] > nums2[second]) {
+            nums1[i] = nums1[first];
+            first--;
+        } else {
+            nums1[i] = nums2[second];
+            second--;
+        }
+
+    }
+};
