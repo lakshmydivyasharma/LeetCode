af02df9b7f9bd71ed0e2dc9c321e80b6c8908683
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-08-30 14:52:24 -0400

    daily leetcode

--- 15_3Sum.js
+++ 15_3Sum.js
@@ -42,39 +42,28 @@ Output: []
     // The sum is equal to 0.
 // once you find it, then reset the pointers and do it again till you hit the end
 
-
-const threeSum = (nums) => {
-  let len = nums.length;
-  if(len < 3) return [];
-  nums.sort(function(a,b){
-    return a-b;
-  })
-  if(nums[0] > 0 || nums[0] + nums[1] + nums[2] > 0) return [];
-  if(len === 3) {
-    if(nums[0] + nums[1] + nums[2] === 0) return [nums];
-    else return [];
-  }
-  //console.log(nums);
-  let result = [];
-  let checker = '';
-  for(let i=0; i<len-2; i++){
-    for(let j=i+1; j<len-1; j++){
-      //since the array is sorted, we only need to start from
-      //the last index that where value was found, since
-      //nums[k] has to be a lower number.
-      for(let start = len-1, k = start; k>i, k>j;) {
-        let triplet = [nums[i], nums[j], nums[k]];
-        let tripletString = '/' + nums[i] + ' ' + nums[j] + ' ' + nums[k] + '/';
-        if(nums[i] + nums[j] === -nums[k] && !checker.includes(tripletString)){
-          result.push(triplet);
-          checker += tripletString;
-          start--;
-          k = start;
-        } else {
-          k--;
-        }
+var threeSum = function(nums) {
+  var solutions = [];
+  var target = 0;
+  nums.sort(function(a, b) {
+      return a - b;
+  });
+  for(var i = 0; i < nums.length - 2; i++) {
+      if(i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      var lo = i + 1;
+      var hi = nums.length - 1;
+      var sum = - nums[i];
+      while(lo < hi) {
+          if(nums[lo] + nums[hi] === sum) {
+              solutions.push([nums[i],nums[lo],nums[hi]]);
+              while (lo < hi && nums[lo] === nums[lo + 1]) lo++;
+              while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+              lo++; hi--;
+          }else if (nums[lo] + nums[hi] > sum) {
+              hi--;
+          }else {
+              lo++;
+          }
       }
-    }
-  }
-  return result;
+      }
 };
