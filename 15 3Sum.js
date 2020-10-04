ff0767836564d8036cfc4e62c447daaf30d2882b
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-04 12:09:22 -0400

    daily code challenge

--- 15 3Sum.js
+++ 15 3Sum.js
@@ -43,27 +43,29 @@ Output: []
 // once you find it, then reset the pointers and do it again till you hit the end
 
 var threeSum = function(nums) {
-  var solutions = [];
-  var target = 0;
-  nums.sort(function(a, b) {
-      return a - b;
-  });
-  for(var i = 0; i < nums.length - 2; i++) {
-      if(i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      var lo = i + 1;
-      var hi = nums.length - 1;
-      var sum = - nums[i];
-      while(lo < hi) {
-          if(nums[lo] + nums[hi] === sum) {
-              solutions.push([nums[i],nums[lo],nums[hi]]);
-              while (lo < hi && nums[lo] === nums[lo + 1]) lo++;
-              while (lo < hi && nums[hi] == nums[hi-1]) hi--;
-              lo++; hi--;
-          }else if (nums[lo] + nums[hi] > sum) {
-              hi--;
-          }else {
-              lo++;
-          }
-      }
-      }
+    var solutions = [];
+    var target = 0;
+    nums.sort(function(a, b) {
+        return a - b;
+    });
+    for(var i = 0; i < nums.length - 2; i++) {
+        if(i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+        var lo = i + 1;
+        var hi = nums.length - 1;
+        var sum = - nums[i];
+        while(lo < hi) {
+            if(nums[lo] + nums[hi] === sum) {
+                solutions.push([nums[i],nums[lo],nums[hi]]);
+                while (lo < hi && nums[lo] === nums[lo + 1]) lo++;
+                while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                lo++; hi--;
+            }else if (nums[lo] + nums[hi] > sum) {
+                hi--;
+            }else {
+                lo++;
+            }
+        }
+        }
 };
+    return solutions;
+}
