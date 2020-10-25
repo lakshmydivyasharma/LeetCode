c64820973f3f868b95eb7e11ad852ac55b34d20b
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-10-25 18:41:33 -0400

    daily code challenge

---
+++ 18 4Sum.js
@@ -0,0 +1,87 @@
+https://leetcode.com/problems/4sum/submissions/
+
+18. 4Sum
+Medium
+
+2486
+
+359
+
+Add to List
+
+Share
+Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Notice that the solution set must not contain duplicate quadruplets.
+
+
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [], target = 0
+Output: []
+
+
+Constraints:
+
+0 <= nums.length <= 200
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Accepted
+362,032
+Submissions
+1,059,824
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+  if (nums.length < 4) return [];
+
+  var len = nums.length;
+  var res = [];
+  var l = 0;
+  var r = 0;
+  var sum = 0;
+
+  nums.sort((a, b) => (a - b));
+
+  for (var i = 0; i < len - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+    if (nums[i] + nums[len - 1] + nums[len - 2] + nums[len - 3] < target) continue;
+
+    for (var j = i + 1; j < len - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+      if (nums[i] + nums[j] + nums[len - 1] + nums[len - 2] < target) continue;
+
+      l = j + 1;
+      r = len - 1;
+
+      while (l < r) {
+        sum = nums[i] + nums[j] + nums[l] + nums[r];
+
+        if (sum < target) {
+          l++;
+        } else if (sum > target) {
+          r--;
+        } else {
+          res.push([nums[i], nums[j], nums[l], nums[r]]);
+          while (l < r && nums[l] === nums[l + 1]) l++;
+          while (l < r && nums[r] === nums[r - 1]) r--;
+          l++;
+          r--;
+        }
+      }
+    }
+  }
+
+  return res;
+};
